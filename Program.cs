dfac5541426cfbd57ef893e699e66437b48be8bd
brunaags <brunazevedoguimaraes@gmail.com>
2019-05-27 17:08:45 -0300

    Initial project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Take.Blip.Client.Console;
+
+namespace bot_flash_cards_blip_sdk_csharp
+{
+    class Program
+    {
+        static int Main(string[] args) => ConsoleRunner.RunAsync(args).GetAwaiter().GetResult();
+    }
+}
